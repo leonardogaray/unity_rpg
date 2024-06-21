c72af78bcc503e82c7af91d98769b03065910735
BlasGG <blasgg04@gmail.com>
2024-06-21 09:29:11 -0300

    Primer commit

---
+++ Assets/Scripts/Player Controller.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+
+    private bool isMoving;
+
+    public Vector2 input;
+
+    private Animator animator;
+
+    public LayerMask solidObjectsLayer;
+    public LayerMask interactableLayer;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void HandleUpdate()
+    {
+        if (!isMoving)
+        {
+            input.x = Input.GetAxisRaw("Horizontal");
+            input.y = Input.GetAxisRaw("Vertical");
+
+            if (input.x != 0) input.y = 0;
+
+            if (input != Vector2.zero)
+            {
+
+                animator.SetFloat("moveX", input.x);
+                animator.SetFloat("moveY", input.y);
+
+                var targetPos = transform.position;
+                targetPos.x += input.x;
+                targetPos.y += input.y;
+
+                if (IsWalkable(targetPos))
+                    StartCoroutine(Move(targetPos));
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+            Interact();
+
+    }
+
+    void Interact()
+    {
+        var facingDir = new Vector3(animator.GetFloat("moveX"), animator.GetFloat("moveY"));
+        var InteractPos = transform.position + facingDir;
+
+        var collider = Physics2D.OverlapCircle(InteractPos, 0.2f, interactableLayer);
+        if (collider != null)
+        {
+            collider.GetComponent<Interactable>()?.Interact();
+        }
+
+    }
+
+    IEnumerator Move(Vector3 targetPos)
+    {
+        isMoving = true;
+
+        while ((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+            yield return null;
+        }
+        transform.position = targetPos;
+
+        isMoving = false;
+    }
+
+    private bool IsWalkable(Vector3 targetPos)
+    {
+        if (Physics2D.OverlapCircle(targetPos, 0.2f, solidObjectsLayer | interactableLayer) !=null)
+        {
+            return false;
+        }
+
+        return true;
+
+    }
+
+}
